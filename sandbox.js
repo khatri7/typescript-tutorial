b4e316d79a9a080d55ed819ebfc0bdc928ad6b8a
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-19 21:43:48 +0530

    feat: lesson 3

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-var inputs = document.querySelectorAll("input");
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(2));
