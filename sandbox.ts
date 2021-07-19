3d8e5fa87604c48254068e5c4a863d494a848f7a
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-19 21:43:32 +0530

    feat: lesson 3

--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,5 @@
-const inputs = document.querySelectorAll("input");
-inputs.forEach((input) => {
-  console.log(input);
-});
+const circ = (diameter: number) => {
+    return diameter  * Math.PI;
+}
+
+console.log(circ(2))
