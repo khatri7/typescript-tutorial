9946dfa12d3551639c874df4db51e2b5d44822f9
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-21 08:29:43 +0530

    feat: lesson 5

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,16 @@
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(2));
+//explicit type
+var fullName;
+var age;
+var isLoggedIn;
+//array
+var ninjas;
+//array with mixed types - union types
+var mixed;
+//union types can be used for normal variables as well
+var uid;
+//objects
+var obj1;
+obj1 = { name: "Abhishek", age: 20 };
+obj1 = [];
+var obj2;
+obj2 = { name: "Abhishek", age: 20 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,24 @@
-const circ = (diameter: number) => {
-    return diameter  * Math.PI;
-}
+//explicit type
+let fullName: string;
+let age: number;
+let isLoggedIn: boolean;
 
-console.log(circ(2))
+//array
+let ninjas: string[];
+
+//array with mixed types - union types
+let mixed: (string | number)[];
+
+//union types can be used for normal variables as well
+let uid: string | number;
+
+//objects
+let obj1: object;
+obj1 = { name: "Abhishek", age: 20 };
+obj1 = [];
+
+let obj2: {
+  name: string;
+  age: number;
+};
+obj2 = { name: "Abhishek", age: 20 };
