fc6515d55ca14751fb793a4741b709b5983bef80
Abhishek Khatri <abhishekkhatri98@gmail.com>
2021-07-22 13:48:11 +0530

    feat: lesson 8

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,17 +1,15 @@
 "use strict";
-//explicit type
-var fullName;
-var age;
-var isLoggedIn;
-//array
-var ninjas;
-//array with mixed types - union types
-var mixed;
-//union types can be used for normal variables as well
-var uid;
-//objects
-var obj1;
-obj1 = { name: "Abhishek", age: 20 };
-obj1 = [];
-var obj2;
-obj2 = { name: "Abhishek", age: 20 };
+var greet;
+greet = function () {
+    console.log("test");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, "20");
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(10, 5);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,24 +1,18 @@
-//explicit type
-let fullName: string;
-let age: number;
-let isLoggedIn: boolean;
+let greet: Function;
 
-//array
-let ninjas: string[];
-
-//array with mixed types - union types
-let mixed: (string | number)[];
-
-//union types can be used for normal variables as well
-let uid: string | number;
-
-//objects
-let obj1: object;
-obj1 = { name: "Abhishek", age: 20 };
-obj1 = [];
-
-let obj2: {
-  name: string;
-  age: number;
+greet = () => {
+  console.log("test");
 };
-obj2 = { name: "Abhishek", age: 20 };
+
+let add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+};
+
+add(5, 10, "20");
+
+const minus = (a: number, b: number) => {
+  return a - b;
+};
+
+let result = minus(10, 5);
